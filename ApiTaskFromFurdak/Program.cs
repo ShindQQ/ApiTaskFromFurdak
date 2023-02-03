2fb473d340bedb55d1768f56379a729e65bc6427
ShindQQ <denis110402@gmail.com>
2023-02-03 16:18:08 +0200

    Added approach using Minimal API

--- ApiTaskFromFurdak/Program.cs
+++ ApiTaskFromFurdak/Program.cs
@@ -1,9 +1,15 @@
+using ApiTask.Bll.Models;
 using ApiTask.Bll.Services;
 using ApiTaskCodeFirst.Dal.Contexts;
+using ApiTaskCodeFirst.Dal.Entities;
 using ApiTaskFromFurdak.CustomMiddlewares;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using Serilog;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Reflection;
 
 Log.Logger = new LoggerConfiguration()
@@ -16,7 +22,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilog();
 
-builder.Services.AddControllers();
+//builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(setupAction =>
@@ -48,6 +54,60 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
+app.MapGet("api/Products/Products", async ([FromServices] IProductsService _productsService) =>
+{
+    return await _productsService.GetAsync();
+});
+
+app.MapGet("api/Products/{id:int}", async ([Required] [FromRoute] int id,
+    [FromServices] IProductsService _productsService) =>
+{
+    return await _productsService.FindProductAsync(new ProductForSearchDto { Id = id });
+});
+
+app.MapGet("api/Products", async ([Required] string category,
+    [FromServices] IProductsService _productsService) =>
+{
+    return await _productsService.FindProductsAsync(new ProductForSearchDto { Category = category });
+});
+
+app.MapPost("api/Products/{id:int}/Quantity", async ([Required] int id, 
+    [Required] [FromBody] int quantity,
+    [FromServices] IProductsService _productsService) =>
+{
+    return await _productsService.ChangeProductQuantityAsync(id, quantity);
+});
+
+app.MapPost("api/Products", async ([Required] [FromBody] ProductForInsertAndUpdateDto product,
+    [FromServices] IMapper _mapper, 
+    [FromServices] IProductsService _productsService) =>
+{
+    return await _productsService.AddAsync(_mapper.Map<Product>(product));
+});
+
+app.MapPost("api/Products/{id:int}/Attribute", async ([Required] int id, 
+    [Required] [FromBody] List<ProductAttribute> productAttributes,
+    [FromServices] IMapper _mapper, 
+    [FromServices] IProductsService _productsService) =>
+{
+    return await _productsService.ChangeProductAttributesAsync(id, productAttributes);
+});
+
+app.MapPut("api/Products/{id:int}/Attribute", async ([Required] int id, 
+    [Required] [FromBody] List<ProductAttribute> productAttributes,
+    [FromServices] IMapper _mapper, 
+    [FromServices] IProductsService _productsService) =>
+{
+    return await _productsService.AddProductAttributesAsync(id, productAttributes);
+});
+
+app.MapDelete("api/Products/{id:int}", async ([Required] int id, 
+    [FromServices] IMapper _mapper, 
+    [FromServices] IProductsService _productsService) =>
+{
+    await _productsService.RemoveAsync(id);
+});
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -63,8 +123,8 @@ app.UseMiddleware<HttpStatusCodeExceptionMiddleware>();
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
-app.MapControllers();
+//app.MapControllers();
 
 app.Run();
